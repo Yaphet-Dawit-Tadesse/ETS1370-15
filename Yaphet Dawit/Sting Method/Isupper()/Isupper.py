5f2d81e9ddbc9329e611aec3885535c993bde8a1
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-20 13:13:19 +0300

    Update Isupper.py

--- Yaphet Dawit/Sting Method/Isupper()/Isupper.py
+++ Yaphet Dawit/Sting Method/Isupper()/Isupper.py
@@ -1,1 +1,13 @@
+Yaph = "DAWIT"L
+print(Yaph.isupper()))  # Output: True
+
+Yaphs = "dawit"
+print(Yaphs.isupper())  # Output: False
+
+Yaphss= "SAMIDAWIT"
+print(Sss.isupper())  # Output: True
+
+empty_string = ""
+print(empty_string.isupper())  # Output: False
+
 
