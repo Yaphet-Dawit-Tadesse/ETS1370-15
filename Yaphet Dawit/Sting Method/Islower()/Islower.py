a9e6bdf3ae78cd5a284b7bf3c4a0fdbff1cd815b
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-20 13:32:32 +0300

    Update Islower.py

--- Yaphet Dawit/Sting Method/Islower()/Islower.py
+++ Yaphet Dawit/Sting Method/Islower()/Islower.py
@@ -1,1 +1,13 @@
 
+
+Y = "yaphet"
+print(YD.islower())  # Output: True
+
+Yy = "Yaphet"
+print(Yy.islower())  # Output: False
+
+Yyy = "yaphetdawit"
+print(Yyy.islower())  # Output: True
+
+empty_string = ""
+print(empty_string.islower())  # Output: False
