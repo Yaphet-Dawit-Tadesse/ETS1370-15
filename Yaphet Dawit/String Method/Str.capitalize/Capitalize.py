799217fce7ae91a094e5784429e2293bc651c167
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-24 15:44:55 +0300

    Delete Yaphet Dawit/String Method/Str.capitalize directory

--- Yaphet Dawit/String Method/Str.capitalize/Capitalize.py
+++
@@ -1,9 +0,0 @@
-word = "cute cats"
-
-# Convert to capitalize
-capitalized_word = word.capitalize()
-
-# Print the result
-print(capitalized_word)  # Output: "Cute cats"
-
-
--- Yaphet Dawit/String Method/Str.capitalize/readme.md
+++
@@ -1,2 +0,0 @@
-The str.capitalize() method in Python is used to convert the first character of a string to uppercase and all other characters to lowercase. 
-
