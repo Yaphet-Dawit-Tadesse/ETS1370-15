5dab21ba32f91c8cd0d74f82a8be5498b63aa0a6
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-27 15:05:55 +0300

    Update Replace.py

--- Yaphet Dawit/Sting Method/Str.replace()/Replace.py
+++ Yaphet Dawit/Sting Method/Str.replace()/Replace.py
@@ -1,1 +1,6 @@
+Y = "I love merry. But merry doesn't love me"
 
+# Replace "merry" with "betty"
+new_text = text.replace("merry", "betty")
+
+print(new_text)  # Output: "I love betty. But betty doesn't love me."
