3bde4b04654f8a2f5b1223ba0f2756f077e39088
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-30 12:27:13 +0300

    Update Isspace.py

--- Yaphet Dawit/Sting Method/Str.Isspace())/Isspace.py
+++ Yaphet Dawit/Sting Method/Str.Isspace())/Isspace.py
@@ -1,1 +1,10 @@
+text = "   "  # Spaces only
 
+# Check if text contains only spaces
+result = text.isspace()
+
+print(result)  # Output: True
+
+
+text = "dance "
+print(text.isspace())  # Output: False
