d3b577da45b9a6eb789ceeb3d3c0dc9926214f69
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-30 12:07:09 +0300

    Update Format.py
    1

--- Yaphet Dawit/Sting Method/Str.format()/Format.py
+++ Yaphet Dawit/Sting Method/Str.format()/Format.py
@@ -1,1 +1,7 @@
+name = "Llinda"
+age = 19-
 
+# Format a string with placeholders
+sentence = "My name is {} and I am {} years old.".format(name, age)
+
+print(sentence)  # Output: "My name is Linda and I am 19 years old."
