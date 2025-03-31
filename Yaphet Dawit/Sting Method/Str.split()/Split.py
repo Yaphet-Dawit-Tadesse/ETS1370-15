edad6954843fda0e6adbd57ee016107557311f74
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-31 12:07:28 +0300

    Update Split.py

--- Yaphet Dawit/Sting Method/Str.split()/Split.py
+++ Yaphet Dawit/Sting Method/Str.split()/Split.py
@@ -1,1 +1,16 @@
+text = "tea milk coffee"
 
+# Split text into a list
+words = text.split()
+
+print(words)  # Output: ['tea', 'milk', 'coffee']
+
+
+or we can specify a custom delimiter
+
+text = "tea,milk,coffee"
+
+# Split using comma
+words = text.split(",")
+
+print(words)  # Output: ['tea', 'milk', 'coffee']
