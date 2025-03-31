18ebef58b466f4db4ea5abe3a22899a20489244c
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-03-31 11:54:38 +0300

    Update Title.py

--- Yaphet Dawit/Sting Method/Str.title()/Title.py
+++ Yaphet Dawit/Sting Method/Str.title()/Title.py
@@ -1,1 +1,7 @@
+sentence = "this boy is such a looser"
 
+# Convert to title case
+title_case_sentence = sentence.title()
+
+# Print the result
+print(title_case_sentence)  # Output: "This Boy Is Such A Looser"
