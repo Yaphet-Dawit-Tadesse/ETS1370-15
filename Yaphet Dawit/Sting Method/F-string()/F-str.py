ce02e65016eb05cca21211dc897f1544adb210f7
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-04-05 18:23:41 +0300

    Update F-str.py

--- Yaphet Dawit/Sting Method/F-string()/F-str.py
+++ Yaphet Dawit/Sting Method/F-string()/F-str.py
@@ -1,1 +1,15 @@
+name = "Khalid"
+age = 21
 
+# Using f-string
+sentence = f"My name is {name} and I am {age} years old."
+
+print(sentence)  # Output: "My name is Khalid and I am 21 years old."
+
+
+or
+
+
+x = 11
+y = 7
+print(f"The sum of {x} and {y} is {x + y}.")  # Output: "The sum of 11 and 7 is 18."
