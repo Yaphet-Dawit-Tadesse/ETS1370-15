53031f92954042938c2ceb9a40548781024158ed
Yaphet-Dawit-Tadesse <ydawit02@gmail.com>
2025-05-22 20:40:01 +0300

    Update Items.py

--- Dictionary methods/Items()/Items.py
+++ Dictionary methods/Items()/Items.py
@@ -1,1 +1,9 @@
-
+food_info = { "name": "cake","color": "white","price": 18,"quantity": 4}
+#Print all key-value pairs
+for key, value in food_info.items():
+    print(f"{key} -> {value}")
+  #output 
+name -> cake
+color -> white
+price -> 18
+quantity -> 4
